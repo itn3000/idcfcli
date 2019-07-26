561e15a5babb519bcef8db0f65c675ead4ff9be3
itn3000 <itn3000@gmail.com>
2019-07-26 12:02:29 +0900

    forget to commit

---
+++ src/compute.rs
@@ -0,0 +1,325 @@
+extern crate base64;
+extern crate clap;
+extern crate crypto;
+extern crate env_logger;
+extern crate log;
+extern crate percent_encoding;
+extern crate reqwest;
+extern crate serde_json;
+extern crate url;
+
+use clap::{App, Arg};
+use crypto::mac::Mac;
+use std::convert::AsRef;
+use std::convert::From;
+use std::io::Read;
+use std::io::Write;
+use std::iter::Iterator;
+
+use super::encode_form_url_utf8;
+use super::ApplicationError;
+use super::InvalidParameter;
+
+struct CommandOptions {
+    method: String,
+    apikey: String,
+    secretkey: String,
+    endpoint: String,
+    output_path: Option<String>,
+}
+
+impl CommandOptions {
+    pub fn output_path(&self) -> Option<&str> {
+        match &self.output_path {
+            Some(v) => Some(v.as_str()),
+            None => None,
+        }
+    }
+}
+
+fn get_command_option(app: &clap::ArgMatches) -> Result<CommandOptions, ApplicationError> {
+    let method = match app.value_of("method") {
+        Some(v) => v.to_owned(),
+        None => {
+            return Err(ApplicationError::ParameterError(InvalidParameter::new(
+                "method",
+                "you must set method option",
+            )))
+        }
+    };
+    let apikey = match app.value_of("apikey") {
+        Some(v) => v.to_owned(),
+        None => match std::env::var("IDCF_API_KEY") {
+            Ok(v) => v,
+            Err(_) => {
+                return Err(ApplicationError::ParameterError(InvalidParameter::new(
+                    "apikey",
+                    "you must set API key option or IDCF_API_KEY env variable",
+                )))
+            }
+        },
+    };
+    let secretkey = match app.value_of("secretkey") {
+        Some(v) => v.to_owned(),
+        None => match std::env::var("IDCF_SECRET_KEY") {
+            Ok(v) => v,
+            Err(_) => {
+                return Err(ApplicationError::ParameterError(InvalidParameter::new(
+                    "apikey",
+                    "you must set API key option or IDCF_SECRET_KEY env variable",
+                )))
+            }
+        },
+    };
+    let endpoint = match app.value_of("endpoint") {
+        Some(v) => v.to_owned(),
+        None => match std::env::var("IDCF_ENDPOINT") {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(ApplicationError::ParameterError(InvalidParameter::new(
+                    "endpoint",
+                    format!("you must set endpoint({:?})", e).as_str(),
+                )))
+            }
+        },
+    };
+    let output_path = match app.value_of("output") {
+        Some(v) => Some(v.to_owned()),
+        None => None,
+    };
+    Ok(CommandOptions {
+        method: method,
+        apikey: apikey,
+        secretkey: secretkey,
+        endpoint: endpoint,
+        output_path: output_path,
+    })
+}
+
+pub fn create_app<'a, 'b>() -> App<'a, 'b> {
+    App::new("compute")
+        .about("IDCF compute API client")
+        .version(super::VERSION_STRING)
+        .arg(
+            Arg::with_name("apikey")
+                .short("a")
+                .long("apikey")
+                .value_name("API_KEY")
+                .help("IDCF api key"),
+        )
+        .arg(
+            Arg::with_name("secretkey")
+                .short("s")
+                .long("secretkey")
+                .value_name("SECRET_KEY")
+                .help("IDCF secret key"),
+        )
+        .arg(
+            Arg::with_name("input-json")
+                .short("i")
+                .long("input")
+                .value_name("INPUT_JSON_FILE")
+                .help("input keyvalue json file(cannot use with 'k' option)")
+                .conflicts_with("keyvalue"),
+        )
+        .arg(
+            Arg::with_name("keyvalue")
+                .short("k")
+                .long("keyvalue")
+                .value_name("KEY_VALUE")
+                .conflicts_with("input-json")
+                .help("query keyvalue pair(A=B)(cannot use with 'i' option)")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("method")
+                .short("m")
+                .long("method")
+                .value_name("METHOD")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("endpoint")
+                .short("e")
+                .long("endpoint")
+                .value_name("END_POINT")
+                .help("if not set, IDCF_ENDPOINT environment variable will be used"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("OUTPUT_PATH")
+                .help("output file path, if not set, output to stdout"),
+        )
+}
+
+fn create_querystring<TKey, TValue>(
+    command: &str,
+    apikey: &str,
+    parameters: &Vec<(TKey, TValue)>,
+) -> String
+where
+    TKey: std::string::ToString + std::cmp::Ord,
+    TValue: std::string::ToString,
+{
+    let mut queryvalues: Vec<(String, String)> = parameters
+        .iter()
+        .map(|(x, y)| (x.to_string(), y.to_string()))
+        .collect();
+    queryvalues.push((String::from("command"), String::from(command)));
+    queryvalues.push(("apikey".to_owned(), apikey.to_owned()));
+    queryvalues.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
+
+    let x: Vec<String> = queryvalues
+        .iter()
+        .map(|(x, y)| {
+            format!(
+                "{}={}",
+                super::encode_form_url_utf8(x),
+                super::encode_form_url_utf8(y)
+            )
+        })
+        .collect();
+    x.join("&")
+}
+
+fn get_keyvalue_from_json_file(filepath: &str) -> Result<Vec<(String, String)>, ApplicationError> {
+    let f = match std::fs::File::open(filepath) {
+        Ok(v) => v,
+        Err(e) => return Err(ApplicationError::IoError(e)),
+    };
+    let v: serde_json::Value = match serde_json::from_reader(f) {
+        Ok(v) => v,
+        Err(e) => return Err(ApplicationError::SerdeError(e)),
+    };
+    match v {
+        serde_json::Value::Object(o) => {
+            let ret: Vec<(String, String)> = o
+                .iter()
+                .map(|(k, v)| (String::from(k), String::from(v.as_str().unwrap())))
+                .collect();
+            Ok(ret)
+        }
+        _ => Err(ApplicationError::IoError(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "invalid json format",
+        ))),
+    }
+}
+
+fn get_keyvalue_from_strings(elements: &[&str]) -> Result<Vec<(String, String)>, ApplicationError> {
+    let ret: Vec<(String, String)> = elements
+        .iter()
+        .filter(|x| x.contains("="))
+        .map(|x| {
+            let splitted: Vec<&str> = x.splitn(2, "=").collect();
+            (splitted[0].to_owned(), splitted[1].to_owned())
+        })
+        .collect();
+    Ok(ret)
+}
+
+fn get_signature(query_string: &str, secret_key: &str) -> Result<String, ApplicationError> {
+    let hash = crypto::sha1::Sha1::new();
+    let mut hmac = crypto::hmac::Hmac::new(hash, secret_key.as_bytes());
+    let inputstr = query_string.to_lowercase().replace("+", "%20");
+    info!("input string = {}", inputstr);
+    hmac.input(inputstr.as_bytes());
+    let hashed = Vec::from(hmac.result().code());
+    return Ok(encode_form_url_utf8(base64::encode(&hashed).as_ref()));
+}
+
+fn get_parameters<'a>(
+    app: &clap::ArgMatches<'a>,
+) -> Result<Vec<(String, String)>, ApplicationError> {
+    match app.value_of("input-json") {
+        Some(v) => Ok(get_keyvalue_from_json_file(v)?),
+        None => match app.values_of("keyvalue") {
+            Some(v) => {
+                let vec: Vec<&str> = v.collect();
+                get_keyvalue_from_strings(&vec)
+            }
+            None => Ok(Vec::new() as Vec<(String, String)>),
+        },
+    }
+}
+
+fn output_response_to_stream<T>(w: &mut T, mut res: reqwest::Response) -> Result<(), std::io::Error>
+where
+    T: std::io::Write,
+{
+    let mut buf = [0u8; 4096];
+    loop {
+        let bytesread = res.read(&mut buf)?;
+        if bytesread <= 0 {
+            break;
+        }
+        w.write(&buf[0..bytesread])?;
+    }
+    Ok(())
+}
+
+fn output_response(
+    res: reqwest::Response,
+    output_path: Option<&str>,
+) -> Result<(), ApplicationError> {
+    if let Some(file_path) = output_path {
+        let mut f = match std::fs::File::create(file_path) {
+            Ok(v) => v,
+            Err(e) => return Err(ApplicationError::IoError(e)),
+        };
+        match output_response_to_stream(&mut f, res) {
+            Ok(_) => (),
+            Err(e) => return Err(ApplicationError::IoError(e)),
+        };
+    } else {
+        let mut stdout = std::io::stdout();
+        match output_response_to_stream(&mut stdout, res) {
+            Ok(_) => (),
+            Err(e) => return Err(ApplicationError::IoError(e)),
+        };
+    }
+    Ok(())
+}
+
+pub fn execute<'a>(app: &clap::ArgMatches<'a>) -> Result<(), ApplicationError> {
+    let mut parameters = get_parameters(&app)?;
+    let option = get_command_option(&app)?;
+    parameters.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
+    let query_string =
+        create_querystring(option.method.as_str(), option.apikey.as_str(), &parameters);
+    let signature = get_signature(&query_string, &option.secretkey)?;
+    let client: reqwest::Client = reqwest::ClientBuilder::new()
+        .use_default_tls()
+        .use_sys_proxy()
+        .redirect(reqwest::RedirectPolicy::default())
+        .build()
+        .unwrap();
+    let requesturl = match reqwest::Url::parse(
+        format!(
+            "{}?{}&signature={}",
+            option.endpoint, query_string, signature
+        )
+        .as_ref(),
+    ) {
+        Ok(v) => v,
+        Err(e) => return Err(ApplicationError::ReqwestParseError(e)),
+    };
+    match client
+        .request(reqwest::Method::GET, requesturl)
+        .header("Accept", "application/json")
+        .send()
+    {
+        Ok(v) => {
+            let v: reqwest::Response = v;
+            info!("request success:{:?}", v);
+            output_response(v, option.output_path())?;
+        }
+        Err(e) => {
+            println!("reqwest error:{:?}", e);
+            return Err(ApplicationError::ReqwestError(e));
+        }
+    };
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 extern crate base64;
 extern crate clap;
 extern crate crypto;
+extern crate env_logger;
 extern crate percent_encoding;
 extern crate reqwest;
 extern crate serde_json;
 extern crate url;
+#[macro_use]
+extern crate log;
+
+const VERSION_STRING: &str = "0.1.0";
 
 use clap::{App, Arg};
 use crypto::mac::Mac;
 use std::convert::AsRef;
 use std::convert::From;
 use std::iter::Iterator;
+use std::io::Write;
+use std::io::Read;
+
+mod compute;
 
 #[derive(Debug)]
-struct InvalidParameter {
+pub struct InvalidParameter {
     name: String,
     description: String,
 }
@@ -28,7 +37,7 @@ impl InvalidParameter {
 }
 
 #[derive(Debug)]
-enum ApplicationError {
+pub enum ApplicationError {
     IoError(std::io::Error),
     ParameterError(InvalidParameter),
     SerdeError(serde_json::Error),
@@ -52,215 +61,29 @@ fn encode_form_url_utf8(value: &str) -> String {
             ret.push('+');
         } else {
             let tmp = format!("%{:<02X}", b);
-            println!("{} = {}", b, tmp);
+            debug!("{} = {}", b, tmp);
             ret.push_str(tmp.as_str());
         }
     }
     ret
 }
 
-fn create_querystring<TKey, TValue>(
-    command: &str,
-    apikey: &str,
-    parameters: &Vec<(TKey, TValue)>,
-) -> String
-where
-    TKey: std::string::ToString + std::cmp::Ord,
-    TValue: std::string::ToString,
-{
-    let mut queryvalues: Vec<(String, String)> = parameters
-        .iter()
-        .map(|(x, y)| (x.to_string(), y.to_string()))
-        .collect();
-    queryvalues.push((String::from("command"), String::from(command)));
-    queryvalues.push(("apikey".to_owned(), apikey.to_owned()));
-    queryvalues.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
-
-    let x: Vec<String> = queryvalues.iter().map(|(x, y)| {
-        format!("{}={}", encode_form_url_utf8(x), encode_form_url_utf8(y))
-    }).collect();
-    x.join("&")
-}
-
 fn create_app<'a, 'b>() -> App<'a, 'b> {
     App::new("IDCF client")
-        .version("0.1.0")
-        .arg(
-            Arg::with_name("apikey")
-                .short("a")
-                .long("apikey")
-                .value_name("API_KEY")
-                .help("IDCF api key"),
-        )
-        .arg(
-            Arg::with_name("secretkey")
-                .short("s")
-                .long("secretkey")
-                .value_name("SECRET_KEY")
-                .help("IDCF secret key"),
-        )
-        .arg(
-            Arg::with_name("input-json")
-                .short("i")
-                .long("input")
-                .value_name("INPUT_JSON_FILE")
-                .help("input keyvalue json file(cannot use with 'k' option)")
-                .conflicts_with("keyvalue"),
-        )
-        .arg(
-            Arg::with_name("keyvalue")
-                .short("k")
-                .long("keyvalue")
-                .value_name("KEY_VALUE")
-                .conflicts_with("input-json")
-                .help("query keyvalue pair(A=B)(cannot use with 'i' option)")
-                .multiple(true),
-        )
-        .arg(
-            Arg::with_name("method")
-                .short("m")
-                .long("method")
-                .value_name("METHOD")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("endpoint")
-                .short("e")
-                .long("endpoint")
-                .required(true)
-                .value_name("END_POINT"),
-        )
-}
-
-fn get_keyvalue_from_json_file(filepath: &str) -> Result<Vec<(String, String)>, ApplicationError> {
-    let f = match std::fs::File::open(filepath) {
-        Ok(v) => v,
-        Err(e) => return Err(ApplicationError::IoError(e)),
-    };
-    let v: serde_json::Value = match serde_json::from_reader(f) {
-        Ok(v) => v,
-        Err(e) => return Err(ApplicationError::SerdeError(e)),
-    };
-    match v {
-        serde_json::Value::Object(o) => {
-            let ret: Vec<(String, String)> = o
-                .iter()
-                .map(|(k, v)| (String::from(k), String::from(v.as_str().unwrap())))
-                .collect();
-            Ok(ret)
-        }
-        _ => Err(ApplicationError::IoError(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            "invalid json format",
-        ))),
-    }
-}
-
-fn get_keyvalue_from_strings(elements: &[&str]) -> Result<Vec<(String, String)>, ApplicationError> {
-    let ret: Vec<(String, String)> = elements
-        .iter()
-        .filter(|x| x.contains("="))
-        .map(|x| {
-            let splitted: Vec<&str> = x.splitn(2, "=").collect();
-            (splitted[0].to_owned(), splitted[1].to_owned())
-        })
-        .collect();
-    Ok(ret)
-}
-
-fn get_signature(query_string: &str, secret_key: &str) -> Result<String, ApplicationError> {
-    let hash = crypto::sha1::Sha1::new();
-    let mut hmac = crypto::hmac::Hmac::new(hash, secret_key.as_bytes());
-    let inputstr = query_string.to_lowercase().replace("+", "%20");
-    println!("input string = {}", inputstr);
-    hmac.input(inputstr.as_bytes());
-    let hashed = Vec::from(hmac.result().code());
-    return Ok(encode_form_url_utf8(base64::encode(&hashed).as_ref()));
-}
-
-fn get_parameters<'a>(
-    app: &clap::ArgMatches<'a>,
-) -> Result<Vec<(String, String)>, ApplicationError> {
-    match app.value_of("input-json") {
-        Some(v) => Ok(get_keyvalue_from_json_file(v)?),
-        None => match app.values_of("keyvalue") {
-            Some(v) => {
-                let vec: Vec<&str> = v.collect();
-                get_keyvalue_from_strings(&vec)
-            }
-            None => Ok(Vec::new() as Vec<(String, String)>),
-        },
-    }
+        .version(VERSION_STRING)
+        .subcommand(compute::create_app())
 }
 
 fn main() -> Result<(), ApplicationError> {
+    env_logger::init();
     let app = create_app().get_matches();
-    let mut parameters = get_parameters(&app)?;
-    let method = match app.value_of("method") {
-        Some(v) => v.to_owned(),
-        None => {
-            return Err(ApplicationError::ParameterError(InvalidParameter::new(
-                "method",
-                "you must set method option",
-            )))
-        }
-    };
-    let apikey = match app.value_of("apikey") {
-        Some(v) => v.to_owned(),
-        None => match std::env::var("IDCF_API_KEY") {
-            Ok(v) => v,
-            Err(_) => {
-                return Err(ApplicationError::ParameterError(InvalidParameter::new(
-                    "apikey",
-                    "you must set API key option or IDCF_API_KEY env variable",
-                )))
-            }
+    match app.subcommand() {
+        ("compute", Some(app)) => {
+            compute::execute(app)
         },
-    };
-    let secretkey = match app.value_of("secretkey") {
-        Some(v) => v.to_owned(),
-        None => match std::env::var("IDCF_SECRET_KEY") {
-            Ok(v) => v,
-            Err(_) => {
-                return Err(ApplicationError::ParameterError(InvalidParameter::new(
-                    "apikey",
-                    "you must set API key option or IDCF_SECRET_KEY env variable",
-                )))
-            }
-        },
-    };
-    let endpoint = match app.value_of("endpoint") {
-        Some(v) => v.to_owned(),
-        None => {
-            return Err(ApplicationError::ParameterError(InvalidParameter::new(
-                "endpoint",
-                "you must set endpoint",
-            )))
+        _ => {
+            error!("unknown subcommand");
+            Err(ApplicationError::ParameterError(InvalidParameter::new("subcommand", "unknown subcommand")))
         }
-    };
-    parameters.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
-    let query_string = create_querystring(method.as_str(), apikey.as_str(), &parameters);
-    let signature = get_signature(&query_string, &secretkey)?;
-    let client: reqwest::Client = reqwest::ClientBuilder::new()
-        .use_default_tls()
-        .use_sys_proxy()
-        .redirect(reqwest::RedirectPolicy::default())
-        .build()
-        .unwrap();
-    let requesturl = match reqwest::Url::parse(
-        format!("{}?{}&signature={}", endpoint, query_string, signature).as_ref(),
-    ) {
-        Ok(v) => v,
-        Err(e) => return Err(ApplicationError::ReqwestParseError(e)),
-    };
-    match client.request(reqwest::Method::POST, requesturl).send() {
-        Ok(v) => {
-            println!("request success:{:?}", v);
-        }
-        Err(e) => {
-            println!("reqwest error:{:?}", e);
-            return Err(ApplicationError::ReqwestError(e));
-        }
-    };
-    Ok(())
+    }
 }
